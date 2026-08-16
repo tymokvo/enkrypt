c7377cc05ea98f72d2a47c48b91a045f8f8e4612
Tyler Kvochick <tyler.kvochick@teecom.com>
2026-08-15 23:50:51 -0400

    Basic encrypt decrypt working

--- crates/enkrypt/src/main.rs
+++ crates/enkrypt/src/main.rs
@@ -1,39 +1,72 @@
+use base64::{Engine, engine::general_purpose::URL_SAFE_NO_PAD as b64};
 use chacha20poly1305::{
     XChaCha20Poly1305, XNonce,
-    aead::{Aead, AeadCore, Generate, Key, KeyInit},
+    aead::{Aead, Generate, KeyInit},
 };
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use sha2::Digest;
 
 #[derive(Parser)]
 struct Args {
+    #[arg(short, long)]
     key: String,
-    payload: String,
+    #[command(subcommand)]
+    mode: Mode,
+}
+
+#[derive(Subcommand)]
+enum Mode {
+    Encrypt { cleartext: String },
+    Decrypt { ciphertext: String },
 }
 
 fn main() -> Result<(), String> {
     let args = Args::parse();
-
     let key = {
         // NOTE: This should be swapped for a key derivation function. There is
         // no salt or multi-round hashing with SHA256, it's just convenient.
         sha2::Sha256::digest(args.key.as_bytes())
     };
     let cipher = XChaCha20Poly1305::new(&key);
-    let nonce = XNonce::generate();
 
-    let cipherbytes = cipher
-        .encrypt(&nonce, args.payload.as_bytes())
-        .map_err(|e| format!("{e}"))?;
+    match args.mode {
+        Mode::Encrypt { cleartext } => {
+            let nonce = XNonce::generate();
 
-    let plainbytes = cipher
-        .decrypt(&nonce, cipherbytes.as_ref())
-        .map_err(|e| format!("{e}"))?;
+            let cipherbytes = cipher
+                .encrypt(&nonce, cleartext.as_bytes())
+                .map_err(|e| format!("{e}"))?;
 
-    let plaintext = String::from_utf8(plainbytes).map_err(|e| format!("{e}"))?;
+            let mut output = vec![];
 
-    dbg!(cipherbytes);
-    dbg!(plaintext);
+            output.append(&mut nonce.to_vec());
+            output.append(&mut cipherbytes.to_vec());
 
-    Ok(())
+            println!("{}", b64.encode(output.as_slice()));
+
+            Ok(())
+        }
+        Mode::Decrypt { ciphertext } => {
+            let bytes = b64.decode(&ciphertext).map_err(|e| format!("{e}"))?;
+
+            let (noncebytes, cipherbytes) = bytes.split_at(24);
+
+            let nonce = if noncebytes.len() == 24 {
+                let nonce_array: [u8; 24] = noncebytes.try_into().map_err(|e| format!("{e}"))?;
+                XNonce::try_from(nonce_array).map_err(|e| format!("{e}"))?
+            } else {
+                return Err("could not reconstruct nonce".into());
+            };
+
+            let clearbytes = cipher
+                .decrypt(&nonce, cipherbytes)
+                .map_err(|e| format!("{e}"))?;
+
+            let cleartext = String::from_utf8(clearbytes).map_err(|e| format!("{e}"))?;
+
+            println!("{}", cleartext);
+
+            Ok(())
+        }
+    }
 }
